139763170a0c0423e71bb6bd506cca1622556897
kzfelder <35270718+kzfelder@users.noreply.github.com>
2018-01-22 10:29:37 -0600

    Personalize user welcome message and display absences based on length of user's name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n\nHello, AttendanceApp!\n");
+
+        // Welcome the user
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Welcome, " + name + "!");
+
+        // Create and output list of absences
+        ArrayList<Integer> absences = new ArrayList<>();
+        Random rand = new Random();
+        for(int i = 0; i < name.length(); i++)
+        {
+            absences.add(rand.nextInt(11));
+        }
+        System.out.println("The elements are: " + absences);
     }
+
+
+
 }
